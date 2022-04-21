5c8f446df5448c1e2ba3d1384ddb48f338d34c9a
MadeJWK <madejayendra@gmail.com>
2022-04-21 22:31:41 +0700

    revisi xendit

--- Controllers/xendit.js
+++ Controllers/xendit.js
@@ -1,8 +1,16 @@
 const xendit = require("xendit-node")
+const axios = require('axios');
 
 class Controller {
     static async xendit(req, res, next) {
+        const { id } = req.params
         try{
+            const apiData = await axios.get("https://indonesia-stock-exchange.vercel.app/api")
+            
+            const data = apiData.data.data
+
+            const found = data.find(element => element.Id == id);
+
             const x = new xendit({
                 secretKey: process.env.SECRET_XENDIT
             });
@@ -13,9 +21,10 @@ class Controller {
     
             const resp = await i.createInvoice({
                 externalID: "TOPUP" + new Date(),
-                amount: Math.floor(Math.random() * (2000000 - 100000 + 1)) + 100000,
+                amount: found.Last * 100,
                 description: "Terima Kasih telah TOP UP",
-                invoice_duration: 86400
+                invoice_duration: 86400,
+                successRedirectURL: "http://localhost:8000"
             });
 
             res.send(resp)
--- router/xendit.js
+++ router/xendit.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const Controller = require('../Controllers/xendit');
 
-router.post("/xendit", Controller.xendit)
+router.post("/xendit/:id", Controller.xendit)
 
 module.exports = router;
